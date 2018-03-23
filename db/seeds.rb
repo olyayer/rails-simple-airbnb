057c777761d9659a794dc42ac6128911e9cd0940
Olya Yeremenko <oyeremenko@gmail.com>
2018-03-23 10:38:14 +0000

    updated loofah gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Cosy & Quirky Garden Flat London',
+  address: '10 Mastodon Gardens London W9 1DT',
+  description: 'A cozy summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 55,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'A must stay flat in Lisbon',
+  address: '15 Amendoeiras st',
+  description: 'A great view at a spacious garden. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 90,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'Misterious flat in Sintra',
+  address: '25 Outubto St, Sintra',
+  description: 'People might have died here but long ago. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 5
+)
+
